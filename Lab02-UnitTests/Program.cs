def334a47031f868d65acaff35c0f8cb8e2e48ba
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-07 14:04:13 -0700

    Added external method placeholders and summary notes

--- Lab02-UnitTests/Program.cs
+++ Lab02-UnitTests/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-
+using System.Net;
 
 namespace Lab02_UnitTests
 {
     public class Program
     {
+        public static decimal Balance = 0;
+        
+        /// <summary>
+        /// Calling the user interface method to activate the application
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //UserInterface();
         }
 
+        /// <summary>
+        /// Returns the value of the current balance
+        /// </summary>
+        /// <returns>decimal</returns>
+        public static decimal ViewBalance(){}
+
+        /// <summary>
+        /// Subtracts money from the balance
+        /// </summary>
+        /// <returns>decimal</returns>
+        public static decimal Withdraw(){}
+
+        /// <summary>
+        /// Adds money to the balance
+        /// </summary>
+        /// <returns>decimal</returns>
+        public static decimal Deposit(){}
+
+        /// <summary>
+        /// Prompts the user for standard ATM operations that links to each of the external methods. 
+        /// </summary>
+        public static void UserInterface(){}
+
+        //Testing the test
         public static string FizzBuzz(int number)
         {
             
