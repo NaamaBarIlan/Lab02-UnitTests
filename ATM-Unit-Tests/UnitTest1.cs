bf82d40e26af20bf77cfddcba68899cff4a0c921
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-07 16:08:34 -0700

    Added Withdraw() method and 3 passing tests.

--- ATM-Unit-Tests/UnitTest1.cs
+++ ATM-Unit-Tests/UnitTest1.cs
@@ -6,6 +6,63 @@ namespace ATM_Unit_Tests
 {
     public class UnitTest1
     {
+        
+        [Fact]
+        public void DoesViewBalanceReturnBalance()
+        {
+            decimal testBalance = 123;
+            Balance = testBalance;
+            Assert.Equal(testBalance, ViewBalance());
+        }
+
+        [Fact]
+        public void CanWithdrawAmountFromBalance()
+        {   
+            //Arrange
+            decimal testBalance = 500;
+            Balance = testBalance;
+            decimal testAmount = 100;
+
+            //Act
+            decimal value = Withdraw(testAmount);
+
+            //Assert
+            Assert.Equal(400, value);
+        }
+
+        [Fact]
+        public void IsAmountGreaterThanBalance()
+        {
+            decimal testBalance = 100;
+            Balance = testBalance;
+            decimal testAmount = 500;
+
+            decimal value = Withdraw(testAmount);
+
+            Assert.Equal(100, value);
+        }
+
+        [Fact]
+        public void IsAmountGreaterThanZero()
+        {
+            decimal testBalance = 500;
+            Balance = testBalance;
+            decimal testAmount = 0;
+
+            decimal value = Withdraw(testAmount);
+
+            Assert.Equal(500, value);
+        }
+        /*
+        [Fact]
+        public void CanReturnFizzFor3()
+        {
+            string fizz = FizzBuzz(3);
+
+            // assert
+            Assert.Equal("Fizz", fizz);
+        }
+
         [Fact]
         public void CanReturnFizzFor3()
         {
@@ -14,6 +71,6 @@ namespace ATM_Unit_Tests
             // assert
             Assert.Equal("3", fizz);
         }
-
+        */
     }
 }
--- Lab02-UnitTests/Program.cs
+++ Lab02-UnitTests/Program.cs
@@ -14,30 +14,67 @@ namespace Lab02_UnitTests
         public static void Main(string[] args)
         {
             //UserInterface();
+            Balance = 5.67M;
+            ViewBalance();
         }
 
         /// <summary>
         /// Returns the value of the current balance
         /// </summary>
         /// <returns>decimal</returns>
-        public static decimal ViewBalance(){}
+        public static decimal ViewBalance()
+        {
+            Console.WriteLine($"Your current balance is: {Balance}");
+            return Balance;
+        }
 
         /// <summary>
         /// Subtracts money from the balance
         /// </summary>
         /// <returns>decimal</returns>
-        public static decimal Withdraw(){}
+        public static decimal Withdraw(decimal amount)
+        {
+            Balance = ViewBalance();
+
+            if (Balance < amount)
+            {
+                return Balance;
+            }
+            else if(amount < 0)
+            {
+                return Balance;
+            }
+            else
+            {
+                Balance -= amount;
+                return Balance;
+            }
+        }
 
         /// <summary>
         /// Adds money to the balance
         /// </summary>
         /// <returns>decimal</returns>
-        public static decimal Deposit(){}
+        public static decimal Deposit(decimal amount)
+        {
+            Balance = ViewBalance();
+
+            if (amount < 0)
+            {
+                return Balance;
+            }
+            else
+            {
+                Balance += amount;
+                return Balance;
+            }
+
+        }
 
         /// <summary>
         /// Prompts the user for standard ATM operations that links to each of the external methods. 
         /// </summary>
-        public static void UserInterface(){}
+        //public static void UserInterface(){}
 
         //Testing the test
         public static string FizzBuzz(int number)
